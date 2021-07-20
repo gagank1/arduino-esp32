3877ce1aab4954c0c706b211121d561eb991bd39
Gagan Kaushik <gagan@utexas.edu>
2021-07-20 03:06:00 -0700

    Removed broken case ETH_PHY_KSZ8081. Now builds on PlatformIO ESP32 release 3.3.0 (IDF v4.3) as a component

--- libraries/WiFi/src/ETH.cpp
+++ libraries/WiFi/src/ETH.cpp
@@ -288,7 +288,8 @@ bool ETHClass::begin(uint8_t phy_addr, int power, int mdc, int mdio, eth_phy_typ
             break;
 #endif
         case ETH_PHY_KSZ8081:
-            eth_phy = esp_eth_phy_new_ksz8081(&phy_config);
+            // eth_phy = esp_eth_phy_new_ksz8081(&phy_config);
+            break;
         default:
             break;
     }
